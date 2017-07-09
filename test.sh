3fafb54cddcebecb5f16f7bdac46f0fb1f1bf32a
O. Lissenberg <olissenberg@rsm.nl>
2017-07-10 00:35:31 +0200

    Add Drush and sites.

--- test.sh
+++ test.sh
@@ -14,9 +14,31 @@ EOF
 mkdir -p ${TMP_DIR}/group_vars 2> /dev/null
 cat << EOF > ${TMP_DIR}/group_vars/webservers
 
+# drupal_mysql_admin_user: root
+# drupal_mysql_admin_password: i_am_root
+
+drupal_composer_tasks: false
+drupal_console_tasks: false
+drupal_drush_tasks: false
+
 drupal_drush_users:
   - drupal
   - root
+
+drupal_sites:
+  - site_owner: debian
+    site_group: www-data
+    site_name: drupal-demo
+    site_domain: drupal-demo.dev
+    site_path: /var/www/drupal-demo
+    site_access_log_path: "/var/www/drupal-demo/drupal-demo.access.log"
+    site_error_log_path: "/var/www/drupal-demo/drupal-demo.error.log"
+    site_account_name: admin
+    site_account_pass: pass
+    site_account_mail: admin@drupal-demo.local
+    mysql_database_name: drupal_demo
+    mysql_user_name: demouser
+    mysql_user_password: demopass
 
 EOF
 
@@ -41,7 +63,8 @@ cat << EOF > ${TMP_DIR}/playbook.yml
 #  vars_prompt:
 #    - name: "drupal_github_oauth"
 #      prompt: "Enter GitHub OAuth"
-#      when: drupal_github_oauth == null
+#      when: drupal_github_oauth == false
+
 
 EOF
 
